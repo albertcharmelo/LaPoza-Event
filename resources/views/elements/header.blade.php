fe4ae9f7e15bdbc85dc3d8c72d4d7d878421c7d0
Albert Charmelo <albertcharmelocontacto@gmail.com>
2023-11-10 04:11:20 -0400

    Agregar función de logout

--- resources/views/elements/header.blade.php
+++ resources/views/elements/header.blade.php
@@ -768,7 +768,8 @@
 								</svg>
 								<span class="ms-2">Inbox </span>
 							</a>
-							<a href="{!! url('/page-login'); !!}" class="dropdown-item ai-icon">
+							<a href="{{ route('logout') }}" onclick="event.preventDefault();
+										  document.getElementById('logout-form').submit();" class="dropdown-item ai-icon">
 								<svg id="icon-logout" xmlns="http://www.w3.org/2000/svg" class="text-danger" width="18"
 									height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
 									stroke-linecap="round" stroke-linejoin="round">
@@ -778,6 +779,9 @@
 								</svg>
 								<span class="ms-2">Logout </span>
 							</a>
+							<form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+								@csrf
+							</form>
 						</div>
 					</li>
 
