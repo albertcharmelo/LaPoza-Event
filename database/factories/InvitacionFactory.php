e2512b6f289c1af6225f0875a36c306dea9e77b9
Albert Charmelo <albertcharmelocontacto@gmail.com>
2023-11-11 17:05:20 -0400

    Create InvitacionFactory.php

---
+++ database/factories/InvitacionFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+
+use App\Models\Invitacion;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Invitacion>
+ */
+class InvitacionFactory extends Factory
+{
+    protected $model = Invitacion::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+
+        $array_menus = ['Menu Fijo con Precio', 'Menu Fijo sin Precio', 'Menu a Elegir con Precio', 'Menu a Elegir sin Precio'];
+        return [
+            'texto' => fake()->text(),
+            'imagen' => fake()->imageUrl(960, 680, 'party', true),
+            'tipo_menu' => fake()->randomElement($array_menus),
+            'creado_por' => User::get()->first()->id,
+            'evento_id' => fake()->numberBetween(1, 10),
+        ];
+    }
+}
