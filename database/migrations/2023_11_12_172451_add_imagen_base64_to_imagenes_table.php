5f8c1d8744660fbadbe40b567b2bc7f715d82d23
Albert Charmelo <albertcharmelo@gmail.com>
2023-11-12 14:08:16 -0400

    new Migrations imagenes

---
+++ database/migrations/2023_11_12_172451_add_imagen_base64_to_imagenes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('imagenes', function (Blueprint $table) {            
+            $table->text('imagen_base64')->after('size');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('imagenes', function (Blueprint $table) {
+            $table->dropColumn('imagen_base64');            
+        });
+    }
+};
