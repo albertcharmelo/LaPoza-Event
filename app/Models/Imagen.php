efb6b1741dd90765ef1ffe3fa992da0f191a5c19
Albert Charmelo <albertcharmelo@gmail.com>
2023-11-12 02:05:15 -0400

    Create Imagen.php

---
+++ app/Models/Imagen.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Imagen extends Model
+{
+    use HasFactory, HasUuids;
+    protected $table = 'imagenes';
+    protected $fillable = [
+        'id',
+        'nombre',
+        'url',
+        'formato',
+        'size',
+        'creado_por',
+        'created_at',
+        'updated_at',
+    ];
+
+
+    public function invitaciones()
+    {
+        return $this->belongsToMany(Invitacion::class, 'invitaciones_imagenes');
+    }
+}
