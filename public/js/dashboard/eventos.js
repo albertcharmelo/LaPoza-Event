90c3968fa7f06111801df30bc549db549b918d3f
Albert Charmelo <albertcharmelo@gmail.com>
2023-11-21 12:55:04 -0400

    Update eventos.js

--- public/js/dashboard/eventos.js
+++ public/js/dashboard/eventos.js
@@ -3,9 +3,14 @@
     var table = $("#eventosTable").DataTable({
         createdRow: function (row, data) {
             $(row).addClass("cursor-pointer");
-            $(row).on("click", function () {
-                window.location.href = "/eventos/" + data.id;
-            });
+            // $(row).on("click", function () {
+            //     window.location.href = "/eventos/" + data.id;
+            // });
+            $(row)
+                .find("td:eq(0), td:eq(1), td:eq(2), td:eq(3)")
+                .on("click", function () {
+                    window.location.href = "/eventos/" + data.id;
+                });
         },
         language: {
             paginate: {
@@ -34,10 +39,12 @@
             { data: "fecha" },
             {
                 data: "status",
-                render: function (data) {
-                    var text = data === 1 ? "Activo" : "Inactivo";
-                    var color = data === 1 ? "text-success" : "text-warning";
-                    return `<span class="${color}">${text}</span>`;
+                render: function (data, type, row) {
+                    let isCheked = data === 1 ? "checked" : "";
+                    return `<td><label class="custom-toggle">
+                            <input type="checkbox" ${isCheked} onchange="changeEstado('${row.id}', this.checked)">
+                            <span class="custom-toggle-slider rounded-circle" data-label-off="NO" data-label-on="SI"></span>
+                        </label></td>`;
                 },
             },
         ],
@@ -57,3 +64,19 @@
         this.nodes().to$().removeClass("selected");
     });
 })(jQuery);
+
+function changeEstado(id, isChecked) {
+    let status = isChecked == true ? "1" : "0";
+    $.ajax({
+        type: "POST",
+        url: "/eventos/changeStatus",
+        data: {
+            id: id,
+            status: status,
+        },
+        headers: {
+            "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+        },
+        success: function (response) {},
+    });
+}
