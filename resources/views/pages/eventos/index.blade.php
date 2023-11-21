e8908ec80586fd6b7953bb9362904c794fba77f9
Albert Charmelo <albertcharmelo@gmail.com>
2023-11-21 12:54:55 -0400

    Update index.blade.php activar eventos

--- resources/views/pages/eventos/index.blade.php
+++ resources/views/pages/eventos/index.blade.php
@@ -1,58 +1,60 @@
 @extends('layouts.dashboard')
-@section('css')
 <!-- Datatable -->
-<link href="{{ asset('vendor/datatables/css/jquery.dataTables.min.css') }}" rel="stylesheet" type="text/css" />
+
+@section('css')
+    <link href="{{ asset('vendor/datatables/css/jquery.dataTables.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('css/dashboard/eventos.css') }}" rel="stylesheet" type="text/css" />
 @endsection
 @section('content')
-<div class="container-fluid">
-    <div class="row page-titles">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item active"><a href="javascript:void(0)">Eventos</a></li>
-            <li class="breadcrumb-item"><a href="javascript:void(0)">Eventos</a></li>
-        </ol>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-                    <h4 class="card-title">Todos los eventos</h4>
-                </div>
-                <div class="card-body px-3">
-                    <div class="table-responsive">
-                        <table id="eventosTable" class="display" style="min-width: 845px">
-                            <thead>
-                                <tr>
-
-                                    <th>Nombre</th>
-                                    <th>Email</th>
-                                    <th>Comensales</th>
-                                    <th>Fecha</th>
-                                    <th>Estado</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-
-                            </tbody>
-                            <tfoot>
-                                <tr>
-                                    <th>Nombre</th>
-                                    <th>Email</th>
-                                    <th>Comensales</th>
-                                    <th>Fecha</th>
-                                    <th>Estado</th>
-                                </tr>
-                            </tfoot>
-                        </table>
+    <div class="container-fluid">
+        <div class="row page-titles">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item active"><a href="javascript:void(0)">Eventos</a></li>
+                <li class="breadcrumb-item"><a href="javascript:void(0)">Eventos</a></li>
+            </ol>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4 class="card-title">Todos los eventos</h4>
                     </div>
-                </div>
+                    <div class="card-body px-3">
+                        <div class="table-responsive">
+                            <table id="eventosTable" class="display" style="min-width: 845px">
+                                <thead>
+                                    <tr>
 
+                                        <th>Nombre</th>
+                                        <th>Email</th>
+                                        <th>Comensales</th>
+                                        <th>Fecha</th>
+                                        <th>Activo</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <th>Nombre</th>
+                                        <th>Email</th>
+                                        <th>Comensales</th>
+                                        <th>Fecha</th>
+                                        <th>Activo</th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 @section('scripts')
-<!-- Datatable -->
-<script src="{{ asset('vendor/datatables/js/jquery.dataTables.min.js') }}" type="text/javascript"></script>
-<script src="{{ asset('js/dashboard/eventos.js') }}" type="text/javascript"></script>
+    <!-- Datatable -->
+    <script src="{{ asset('vendor/datatables/js/jquery.dataTables.min.js') }}" type="text/javascript"></script>
+    <script src="{{ asset('js/dashboard/eventos.js') }}" type="text/javascript"></script>
 @endsection
