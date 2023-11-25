26492ce64c7673539292000b4dacb504903f39ea
Albert Charmelo <albertcharmelo@gmail.com>
2023-11-24 20:45:23 -0400

    Update eventosDetails.js, table resumen de platos por evento

--- public/js/dashboard/eventosDetails.js
+++ public/js/dashboard/eventosDetails.js
@@ -60,4 +60,53 @@
     table.rows().every(function () {
         this.nodes().to$().removeClass("selected");
     });
+
+    var tableResumen = $("#TableResumenPlatos").DataTable({
+        createdRow: function (row, data) {          
+            // $(row).on("click", function () {
+            //     window.location.href = "/qrcode/invitacion/" + data.id;
+            // });            
+        },        
+        language: {
+            paginate: {
+                next: '<i class="fa fa-angle-double-right" aria-hidden="true"></i>',
+                previous:
+                    '<i class="fa fa-angle-double-left" aria-hidden="true"></i>',
+            },
+            searchPlaceholder: "Buscar plato",
+            search: "Buscar:",
+            info: "Mostrando _START_ a _END_ de _TOTAL_ platos",
+            lengthMenu: "Mostrar _MENU_ platos",
+        },
+        ajax: {
+            url: "/invitaciones/getPlatos",
+            type: "POST",
+            data: function (d) {
+                d.evento_id = evento_id;
+            },
+            headers: {
+                "X-CSRF-TOKEN": $('meta[name="csrf-token"]').attr("content"),
+            },
+            dataType: "json",
+            dataSrc: "",
+        },
+        columns: [
+            { data: "plato" },
+            { data: "cantidad", className: "dt-body-center" },
+        ],
+    });
+
+    tableResumen.on("click", "tbody tr", function () {
+        var $row = tableResumen.row(this).nodes().to$();
+        var hasClass = $row.hasClass("selected");
+        if (hasClass) {
+            $row.removeClass("selected");
+        } else {
+            $row.addClass("selected");
+        }
+    });
+
+    tableResumen.rows().every(function () {
+        this.nodes().to$().removeClass("selected");
+    });
 })(jQuery);
