d3f9447516c0892ca3269096b37f2891838330c7
Albert Charmelo <albertcharmelo@gmail.com>
2023-12-06 19:54:51 -0400

    Update index.blade.php

--- resources/views/pages/invitacion/index.blade.php
+++ resources/views/pages/invitacion/index.blade.php
@@ -11,7 +11,7 @@
             <div class="col-12">
                 <div class="card">
                     <div class="card-header">
-                        <h4 class="card-title">Invitaciones</h4>
+                        <h4 class="card-title">Todas las Invitaciones</h4>
                     </div>
                     <div class="card-body px-3">
                         <div class="table-responsive">
