d26a51b2aeb6ae3719b2582c8e0ec8a1a2d85e35
Albert Charmelo <albertcharmelocontacto@gmail.com>
2023-12-06 21:10:08 -0400

    Cambios visuales

--- resources/views/pages/eventos/index.blade.php
+++ resources/views/pages/eventos/index.blade.php
@@ -2,58 +2,52 @@
 <!-- Datatable -->
 
 @section('css')
-    <link href="{{ asset('vendor/datatables/css/jquery.dataTables.min.css') }}" rel="stylesheet" type="text/css" />
-    <link href="{{ asset('css/dashboard/eventos.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('vendor/datatables/css/jquery.dataTables.min.css') }}" rel="stylesheet" type="text/css" />
+<link href="{{ asset('css/dashboard/eventos.css') }}" rel="stylesheet" type="text/css" />
 @endsection
 @section('content')
-    <div class="container-fluid">
-        <div class="row page-titles">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item active"><a href="javascript:void(0)">Eventos</a></li>
-                <li class="breadcrumb-item"><a href="javascript:void(0)">Eventos</a></li>
-            </ol>
-        </div>
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h4 class="card-title">Todos los eventos</h4>
-                    </div>
-                    <div class="card-body px-3">
-                        <div class="table-responsive">
-                            <table id="eventosTable" class="display" style="min-width: 845px">
-                                <thead>
-                                    <tr>
-                                        <th with="50%">Nombre</th>
-                                        <th with="35%">Email</th>
-                                        <th with="5%" class="text-center">Comensales</th>
-                                        <th with="5%" class="text-center">Fecha</th>
-                                        <th with="5%" class="text-center">Activo</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-
-                                </tbody>
-                                <tfoot>
-                                    <tr>
-                                        <th with="50%">Nombre</th>
-                                        <th with="35%">Email</th>
-                                        <th with="5%" class="text-center">Comensales</th>
-                                        <th with="5%" class="text-center">Fecha</th>
-                                        <th with="5%" class="text-center">Activo</th>
-                                    </tr>
-                                </tfoot>
-                            </table>
-                        </div>
-                    </div>
-
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Todos los eventos</h4>
                 </div>
+                <div class="card-body px-3">
+                    <div class="table-responsive">
+                        <table id="eventosTable" class="display" style="min-width: 845px">
+                            <thead>
+                                <tr>
+                                    <th with="50%">Nombre</th>
+                                    <th with="35%">Email</th>
+                                    <th with="5%" class="text-center">Comensales</th>
+                                    <th with="5%" class="text-center">Fecha</th>
+                                    <th with="5%" class="text-center">Activo</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                            <tfoot>
+                                <tr>
+                                    <th with="50%">Nombre</th>
+                                    <th with="35%">Email</th>
+                                    <th with="5%" class="text-center">Comensales</th>
+                                    <th with="5%" class="text-center">Fecha</th>
+                                    <th with="5%" class="text-center">Activo</th>
+                                </tr>
+                            </tfoot>
+                        </table>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
+</div>
 @endsection
 @section('scripts')
-    <!-- Datatable -->
-    <script src="{{ asset('vendor/datatables/js/jquery.dataTables.min.js') }}" type="text/javascript"></script>
-    <script src="{{ asset('js/dashboard/eventos.js') }}" type="text/javascript"></script>
+<!-- Datatable -->
+<script src="{{ asset('vendor/datatables/js/jquery.dataTables.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('js/dashboard/eventos.js') }}" type="text/javascript"></script>
 @endsection
