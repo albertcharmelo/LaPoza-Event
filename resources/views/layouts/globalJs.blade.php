926f85a37931e3c43da92d24cf7e60571a9015c3
Albert Charmelo <albertcharmelo@gmail.com>
2023-12-22 01:10:28 -0400

    Create globalJs.blade.php

---
+++ resources/views/layouts/globalJs.blade.php
@@ -0,0 +1,4 @@
+<script src="https://unpkg.com/imask"></script>
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+<script src="{{ asset('vendor/sweetalert2/dist/sweetalert2.min.js') }}" type="text/javascript"></script>
+<script src="{{ asset('vendor/carrusel/dist/owl.carousel.js') }}"></script>
