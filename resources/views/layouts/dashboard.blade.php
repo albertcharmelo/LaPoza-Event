6a53be154c2db04975530596b55875977796edc2
Albert Charmelo <albertcharmelo@gmail.com>
2023-12-22 01:09:34 -0400

    Update dashboard.blade.php, Js Carrusel de imagenes

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -229,7 +229,7 @@ $action = DzHelper::action();
     <script src="{{ asset($script) }}" type="text/javascript"></script>
     @endforeach
     @endif
-    <script src="{{ asset('vendor/OwlCarousel2-2.3.4/dist/owl.carousel.min.js') }}"></script>
+    @include('layouts.globalJs')
     <script src="{{ asset('vendor/toastr/js/toastr.min.js') }}"></script>
     <script src="{{ asset('js/plugins-init/toastr-init.js') }}"></script>
     <script src="{{ asset('js/utils.js') }}"></script>
